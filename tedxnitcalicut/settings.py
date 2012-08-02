e46a5740afb6fee03bc4024cabc0a4f3e700b436
Kartik Singhal <kartiksinghal@gmail.com>
2012-08-02 18:02:04 +0530

    db details modified

--- tedxnitcalicut/settings.py
+++ tedxnitcalicut/settings.py
@@ -20,9 +20,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'tedxnitcalicut',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': 'toor',                  # Not used with sqlite3.
+        'NAME': 'tedxnitc13',                      # Or path to database file if using sqlite3.
+        'USER': 'tedxuser',                      # Not used with sqlite3.
+        'PASSWORD': 'tedxuser',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
